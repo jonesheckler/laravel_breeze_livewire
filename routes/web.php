e4e23f99142b754b74e5b8f20cfa956523637882
Jones Heckler <mw10@mw10.com.br>
2022-04-05 08:49:59 -0300

    install livewire

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
+
 require __DIR__.'/auth.php';
